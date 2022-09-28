e7a227959abd23ba46c309ca16ea50571abb9a60
pufick <98830871+pufick@users.noreply.github.com>
2022-09-28 12:38:24 +0300

    Update hometask2.py

--- multi threading/hometask2.py
+++ multi threading/hometask2.py
@@ -1,11 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import threading
 import time
 import random
-class First:
+class Value:
     def __init__(self, value):
         self.value = value
         self.mutex = threading.Lock()
@@ -16,19 +12,8 @@ class First:
         self.value += term
         self.mutex.release()
 
-class Second:
-    def __init__(self, value):
-        self.value = value
-        self.mutex = threading.Lock()
-    def read(self):
-        return self.value
-    def write(self, term):
-        self.mutex.acquire()
-        self.value += term
-        self.mutex.release()
-
-first_obj = First(0)
-second_obj = Second(0)
+first_obj = Value(0)
+second_obj = Value(0)
 
 def increase(K):
     for i in range(K):
@@ -55,4 +40,5 @@ for thread in threads:
     thread.join()
 
 print(time.time() - start)
-
+print(first_obj.value)
+print(second_obj.value)
