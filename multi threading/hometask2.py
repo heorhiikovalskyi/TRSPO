5bfdd71133a1014e9cf98d4741757f6d01b6230b
pufick <98830871+pufick@users.noreply.github.com>
2022-09-28 12:48:43 +0300

    Update hometask2.py

--- multi threading/hometask2.py
+++ multi threading/hometask2.py
@@ -1,25 +1,28 @@
 import threading
 import time
 import random
+mutex1 = threading.Lock()
+mutex2 = threading.Lock()
 class Value:
     def __init__(self, value):
         self.value = value
-        self.mutex = threading.Lock()
     def read(self):
         return self.value
     def write(self, term):
-        self.mutex.acquire()
         self.value += term
-        self.mutex.release()
 
 first_obj = Value(0)
 second_obj = Value(0)
 
 def increase(K):
     for i in range(K):
+        mutex1.acquire()
         first_obj.write(random.uniform(10.5, 75.5))
+        mutex1.release()
+        mutex2.acquire()
         second_obj.write(random.uniform(10.5, 75.5))
-
+        mutex2.release()
+        
 count = random.randint(10,20)
 threads = []
 K1 = random.randint(10000, 20000)
